398e8a4dbae37984a24fc59f3017ee68c809c1f2
sumandari <sumandari.ee@gmail.com>
2020-01-06 11:55:40 +0800

    first commit

---
+++ pypi_org/services/package_service.py
@@ -0,0 +1,29 @@
+from typing import List
+import sqlalchemy.orm
+
+import pypi_org.data.db_session as db_session
+from pypi_org.data.package import Package
+from pypi_org.data.releases import Release
+
+
+def get_latest_packages(limit=10) -> List[Release]:
+    session = db_session.create_session()
+
+    releases = session.query(Release). \
+        order_by(Release.created_date.desc()).\
+        limit(limit).\
+        all()
+
+    return releases
+
+
+def get_package_count() -> int:
+    return 200
+    session = db_session.create_session()
+    return session.query(Package).count()
+
+
+def get_release_count() -> int:
+    return 200
+    session = db_session.create_session()
+    return session.query(Release).count()
